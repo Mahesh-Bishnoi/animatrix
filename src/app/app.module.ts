86ce4a2f17705adaf8ef70d4bcdce40d5ea0c5ce
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2022-06-26 17:28:04 +0530

    Removed default angular code and added navbar component
    Removed default angular code and added navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from './shared/shared.module';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    AdminModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
