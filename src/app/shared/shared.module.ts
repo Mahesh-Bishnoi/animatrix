a7074a4e2fde694cdbede066ad4058024a03be83
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2022-06-29 21:56:34 +0530

    Added footer component skeleton in shared module
    Added footer component skeleton in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
