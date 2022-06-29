77a7ba1f0dda2fd5ba3544be16c3b17a55ddb0b2
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2022-06-29 21:57:13 +0530

    Added header component skeleton in shared module
    Added header component skeleton in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
