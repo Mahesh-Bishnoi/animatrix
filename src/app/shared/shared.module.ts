48bd5a05fd6df31563e239e90af5c3ee1a0c2e67
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2022-06-30 23:20:34 +0530

    Header title as a dynamic attribute
    Header title as a dynamic attribute

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,7 +9,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule,
   ],
   exports: [
     NavbarComponent,
