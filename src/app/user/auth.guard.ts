653b741a220f08839442752f6f9482b5c80fd255
Mahesh Bishnoi <bishnoimahesh96@gmail.com>
2022-07-06 13:26:41 +0530

    Added auth guard skeleton
    Added auth guard skeleton

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      console.log('AuthGuard#canActivate called');
+      const url: string = state.url;
+
+    return this.checkLogin(url);
+    return false;
+  }
+  checkLogin(url: string): true|UrlTree {
+    if (this.authService.isLoggedIn) { return true; }
+
+    // Store the attempted URL for redirecting
+    this.authService.redirectUrl = url;
+
+    // Redirect to the login page
+    return this.router.parseUrl('/login');
+  }
+}
